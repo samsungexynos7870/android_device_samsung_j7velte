4d1ab9e67b0f0405efc6199f197d6eaf44135607
Brian Gotte <bngotte@gmail.com>
2021-08-04 17:47:34 -0300

    Commonize build fingerprint

--- init/init_j7velte.cpp
+++ init/init_j7velte.cpp
@@ -108,7 +108,6 @@ void vendor_load_properties()
     }
 
     /* Common properties*/
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys");
     property_override("ro.build.description", "samsung/j7veltedx/j7velte:9/PPR1.180610.011/J701FDDU8CTA3:user/release-keys");
     property_override_quad("ro.product.device", "ro.product.odm.device", "ro.product.system.device", "ro.product.vendor.device", "j7velte");
 
